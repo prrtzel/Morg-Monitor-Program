aab92aa897536da6cfa8a83f4a5fc85f9638eed4
prrtzel <gsyacoubian@gmail.com>
2024-06-29 15:03:53 -0400

    docs

--- test/conversions.h
+++ test/conversions.h
@@ -3,7 +3,28 @@
 #define CONVERSIONS_H
 #include <stdint.h>
 
+/**
+ * @brief Converts ascii hex string to binary 
+ * @param ascii_buffer 
+ * @param length 
+ * @return uint32_t
+ */
 extern uint32_t ascii_hex_to_bin(const char* ascii_buffer, const int length);
+
+/**
+ * @brief Converts ascii decimal string to binary
+ *
+ * @param ascii_char
+ * @return char
+ */
 extern unsigned char ascii_decimal_to_bin(const char ascii_char);
+
+/**
+ * @brief Converts a binary number to ascii hex
+ *
+ * @param binary_value raw value
+ * @param str_buffer buffer to place the string in
+ * @param length number of characters to produce
+ */
 extern void binary_to_ascii_hex(const uint32_t binary_value, char* str_buffer, const unsigned char length);
 #endif
--- test/main.c
+++ test/main.c
@@ -3,9 +3,5 @@
 
 int main(void)
 {
-	get_string();
-	uint32_t var = ascii_hex_to_bin(input_buffer, 8);
-	binary_to_ascii_hex(var, output_buffer, 8);
-	serial_print(output_buffer);
 	return 0;
-}	
+}	// main
