f165fd182faee8d6b56c21bf2b6c7e9d08187332
prrtzel <gsyacoubian@gmail.com>
2024-07-10 12:07:15 -0400

    impact deploy bat file implemented, ROM test passed

--- hw_tests/deploy.bat
+++ hw_tests/deploy.bat
@@ -17,15 +17,23 @@ if "%hasDir%"=="false" (
 
 echo "Deploying %name%..."
 
+rem Start easybin and save the file as a .bin
 start /b "" easybin %name%/%name%.S68 /r /e
-
 powershell -nop -c "& {sleep -m 10}"
 wscript "save_bin.vbs" %1.BIN
 taskkill /f /im easybin.exe
 
+rem Start xgpro and open the file 
 start xgpro
-
 powershell -nop -c "& {sleep -m 100}"
 wscript "open_bin_xgpro.vbs" %1.BIN
+powershell -nop -c "& {sleep -m 100}"
+
+rem Start impact and go through pop up window
+start impact
+powershell -nop -c "& {sleep -m 100}"
+wscript "impact_nav.vbs"
+
+echo "Done!"
 
 exit /b
